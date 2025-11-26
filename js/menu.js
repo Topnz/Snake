edd2a65911edbcbfd49b70314a86f30b91670edd
iMannys <peteregeberg123@gmail.com>
2025-11-26 18:21:30 +0100

    New snake menu and localStorage

---
+++ js/menu.js
@@ -0,0 +1,78 @@
+let easyButton = document.querySelector(".easy");
+let intermediateButton = document.querySelector(".intermediate");
+let hardButton = document.querySelector(".hard");
+let extremeButton = document.querySelector(".extreme");
+let impossibleButton = document.querySelector(".impossible");
+
+let highscoreElement = document.querySelector("#highscore");
+
+let highscore = localStorage.getItem("highscore");
+highscoreElement.textContent = "Highscore: " + highscore;
+
+//Opens menuscreen on first load
+gameContainer.classList.add("hidden");
+menuContainer.classList.remove("hidden");
+
+/* Difficulty buttons setting different game speeds */
+
+let gameInterval = null;
+let previousButton = null;
+
+let difficultyStorage = localStorage.getItem("difficulty");
+if (difficultyStorage == null) {
+  difficultyStorage = "easy";
+}
+buttonClicked(difficultyStorage);
+
+function buttonPressedStyle(button) {
+  if (previousButton != null) {
+    previousButton.classList.remove("pressed");
+  }
+  button.classList.add("pressed");
+  previousButton = button;
+}
+
+function buttonClicked(event) {
+  if (gameInterval != null) {
+    clearInterval(gameInterval);
+  }
+
+  let difficulty = null;
+  if (event.target == null) {
+    difficulty = difficultyStorage;
+  } else difficulty = event.target.classList[0];
+
+  switch (difficulty) {
+    case "impossible":
+      gameInterval = setInterval(renderGame, 50);
+      buttonPressedStyle(impossibleButton);
+      localStorage.setItem("difficulty", "impossible");
+      break;
+    case "extreme":
+      gameInterval = setInterval(renderGame, 75);
+      buttonPressedStyle(extremeButton);
+      localStorage.setItem("difficulty", "extreme");
+      break;
+    case "hard":
+      gameInterval = setInterval(renderGame, 125);
+      buttonPressedStyle(hardButton);
+      localStorage.setItem("difficulty", "hard");
+      break;
+    case "intermediate":
+      gameInterval = setInterval(renderGame, 150);
+      buttonPressedStyle(intermediateButton);
+      localStorage.setItem("difficulty", "intermediate");
+      break;
+    default:
+      gameInterval = setInterval(renderGame, 200);
+      buttonPressedStyle(easyButton);
+      localStorage.setItem("difficulty", "easy");
+      break;
+  }
+}
+
+easyButton.addEventListener("click", buttonClicked);
+intermediateButton.addEventListener("click", buttonClicked);
+hardButton.addEventListener("click", buttonClicked);
+extremeButton.addEventListener("click", buttonClicked);
+impossibleButton.addEventListener("click", buttonClicked);
---
+++ js/snake.js
@@ -0,0 +1,170 @@
+let gameContainer = document.querySelector(".game-container");
+let scoreContainer = document.querySelector(".score-container");
+let menuContainer = document.querySelector(".menu-container");
+
+let spawnX, spawnY;
+let headX = null,
+  headY = headX;
+let directionX = 0,
+  directionY = 0;
+let snakeBody = [];
+let score = 0;
+let gameStarted = false;
+let gameOverFinished = true;
+
+function spawnFood() {
+  /** Math.floor to set random number to a whole number rounded down. Indexing at 0 hence +1 */
+  spawnX = Math.floor(Math.random() * 20) + 1;
+  spawnY = Math.floor(Math.random() * 18) + 1;
+
+  /** Loops through spawning places making sure there isn't a part of the snake
+        and then spawns in a different location if there is a part of the snake there */
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeBody[i][1] == spawnY && snakeBody[i][0] == spawnX) {
+      spawnFood();
+    }
+  }
+}
+
+function start() {
+  headX = 5;
+  headY = 9;
+  spawnX = 16;
+  spawnY = 9;
+  snakeBody = [
+    [5, 9],
+    [4, 9],
+    [3, 9],
+  ];
+
+  gameContainer.classList.remove("hidden");
+  menuContainer.classList.add("hidden");
+}
+
+function gameOver() {
+  gameStarted = false;
+  gameOverFinished = false;
+  // Check om highscore er null til at starte med
+  // Ellers check om den er højere end sidst
+  let highscore = localStorage.getItem("highscore");
+  if (highscore == null || score > highscore) {
+    localStorage.setItem("highscore", score);
+    highscoreElement.textContent = "Highscore: " + score;
+  }
+
+  directionX = 0;
+  directionY = 0;
+  snakeBody = [];
+  score = 0;
+  scoreContainer.innerHTML = "Press any button to start snaking!";
+  audio.pause();
+  audio.currentTime = 0;
+
+  //Game over sound effect
+  let audioGameOver = new Audio("audio/game_over.mp3");
+  audioGameOver.play();
+
+  setTimeout(() => {
+    alert("Game Over!");
+    gameContainer.innerHTML = '<div class="game-container">';
+    gameContainer.classList.add("hidden");
+    menuContainer.classList.remove("hidden");
+    gameOverFinished = true;
+  }, 200);
+}
+
+/** Food random generating on load/refresh using change of CSS style.
+    Grid size is X-Axis = 20 * Y-Axis = 18 */
+function renderGame() {
+  if (!gameStarted) return;
+  let updatedGame = `<div class="food" style="grid-area: ${spawnY}/${spawnX};"></div>`;
+  if (spawnX == headX && spawnY == headY) {
+    snakeBody.push([spawnY, spawnX]);
+    spawnFood();
+    score += 1;
+    scoreContainer.innerHTML = "Score: " + score;
+
+    //Eating sound effect
+    let audioEat = new Audio("audio/eatingfood.mp3");
+    audioEat.play();
+  }
+
+  headX += directionX;
+  headY += directionY;
+
+  snakeBody.pop();
+
+  snakeBody.unshift([headX, headY]);
+
+  /** Checks for border collision */
+  if (headX == 0 || headY == 0 || headX == 20 + 1 || headY == 18 + 1) {
+    gameOver();
+    return;
+  }
+
+  /** Checks for collision on snake body */
+  for (let i = 1; i < snakeBody.length; i++) {
+    if (
+      snakeBody[0][0] == snakeBody[i][0] &&
+      snakeBody[0][1] == snakeBody[i][1]
+    ) {
+      gameOver();
+      return;
+    }
+  }
+
+  /** Adds to the lenght of the snake's array */
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (i == 0) {
+      updatedGame += `<div class="snake-head" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]};"></div>`;
+    } else {
+      updatedGame += `<div class="snake" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]};"></div>`;
+    }
+  }
+
+  gameContainer.innerHTML = updatedGame;
+}
+
+spawnFood();
+
+//Background music for the game
+let audio = new Audio("audio/background-music.mp3");
+audio.loop = true;
+audio.volume = 0.25;
+
+document.addEventListener("keydown", function (e) {
+  if (
+    !gameStarted &&
+    gameOverFinished &&
+    [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+      "w",
+      "a",
+      "s",
+      "d",
+    ].includes(e.key)
+  ) {
+    gameStarted = true;
+    start();
+    audio.play();
+  }
+
+  scoreContainer.innerHTML = "Score: " + score;
+  let key = e.key;
+  if ((key == "ArrowUp" || key == "w") && directionY != 1) {
+    directionX = 0;
+    directionY = -1;
+  } else if ((key == "ArrowLeft" || key == "a") && directionX != 1) {
+    directionX = -1;
+    directionY = 0;
+  } else if ((key == "ArrowDown" || key == "s") && directionY != -1) {
+    directionX = 0;
+    directionY = 1;
+  } else if ((key == "ArrowRight" || key == "d") && directionX != -1) {
+    directionX = 1;
+    directionY = 0;
+  }
+});
